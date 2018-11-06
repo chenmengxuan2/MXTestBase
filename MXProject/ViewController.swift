22fada8b5bcdeb48976e2ab36ecc162be697f5f8
木头人 <woodenmanup@yeah.net>
2018-11-06 15:48:35 +0800

    first commit

---
+++ MXProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MXProject
+//
+//  Created by Mengxuan Chen on 2018/11/6.
+//  Copyright © 2018年 Mengxuan Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
